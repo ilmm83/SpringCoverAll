d7e1c126bf797d2f426b1b0226146e308e66f07a
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-15 22:15:55 +0300

    Visual refactoring,a little

--- src/main/java/com/example/SpringCoverAll/controller/TemplateController.java
+++ src/main/java/com/example/SpringCoverAll/controller/TemplateController.java
@@ -1,7 +1,6 @@
 package com.example.SpringCoverAll.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
-public class TemplateController { // todo: Authorization isn't work for this api (Always forbidden)
+public class TemplateController {
 
     @GetMapping("home_page")
     public String toHomePage() {
