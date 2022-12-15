19c5f6678456c21bce4217c736c77d9d0a2afe61
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-15 21:27:03 +0300

    TemplateController performance restored
    Changed annotation to RestController and remove roles based api accesability

--- src/main/java/com/example/SpringCoverAll/controller/TemplateController.java
+++ src/main/java/com/example/SpringCoverAll/controller/TemplateController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class TemplateController { // todo: Authorization isn't work for this api (Always forbidden)
