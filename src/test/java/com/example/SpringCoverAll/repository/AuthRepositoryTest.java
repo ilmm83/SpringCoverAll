a8145d411639f1f3088d44110ee9a76585006ad2
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-16 17:57:32 +0300

    add Test for repo with h2 database

---
+++ src/test/java/com/example/SpringCoverAll/repository/AuthRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.SpringCoverAll.repository;
+
+import com.example.SpringCoverAll.model.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+class AuthRepositoryTest {
+
+    @Autowired
+    private AuthRepository repository;
+
+    @Test
+    void existsByUsername() {
+        Student student = new Student();
+        student.setUsername("user1");
+        student.setPassword("pass1");
+
+        repository.save(student);
+
+        String expect = repository.findByUsername(student.getUsername()).get().getUsername();
+        assertThat(expect).isEqualTo("user1");
+    }
+
+    @Test
+    void findByUsername() {
+    }
+}
